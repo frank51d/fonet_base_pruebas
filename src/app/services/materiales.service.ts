e80c46dce39c5dacbcfaff0402e2b448aeea8b18
frank51d <frank52xd@gmail.com>
2020-02-12 11:14:05 -0400

    listo

--- src/app/services/materiales.service.ts
+++ src/app/services/materiales.service.ts
@@ -14,7 +14,7 @@ import { Plataforma } from '../models/plataforma';
 })
 export class MaterialesService {
 
-  API_URI = 'https://fonet-df.herokuapp.com/api';
+  API_URI = 'https://fonetdf.herokuapp.com/api';
 
   constructor(private http:HttpClient) { }
 
