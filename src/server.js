ab285eecbfe1a1d834cfda86830df7d2d38f84ea
Francisco Perez <frank51xd@gmail.com>
2020-02-12 02:32:07 -0400

    finalddd

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.static(__dirname+'/dist/fonetApp'));
+app.use(express.static('./dist/fonetApp'));
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/fonetApp/index.html'));
